0aa8bebd319c0d123b8a9afd9d0eb69899194152
Gregory Stanek <stanekg@flip3.engr.oregonstate.edu>
2019-06-11 16:42:55 -0700

    Did this work?

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_framet',
-  password        : '8365',
-  database        : 'cs340_framet'
+  user            : 'cs340_stanekg',
+  password        : 'KohU!kLF7i',
+  database        : 'cs340_stanekg',
+  dateStrings 	  : 'date'
 });
 module.exports.pool = pool;
--- public/select.js
+++ public/select.js
@@ -1,3 +1,23 @@
 function selectCity(id){
     $("#city-selector").val(id);
 }
+
+function selectActivities(id){
+	$("#activity-selector").val(id);
+}
+
+function selectAirline(id){
+	$("#airline-selector").val(id);
+}
+
+function selectHotel(id){
+    $("#hotel-selector").val(id);
+}
+
+function selectActivityPrice(id){
+	$("#activity-price-selector").val(id);
+}
+
+function selectDestinationType(id){
+	$("#destination-type-selector").val(id);
+}
